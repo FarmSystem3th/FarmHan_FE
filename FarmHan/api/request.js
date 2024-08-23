51e24a5953c9488ca28699a9990d7cf576925cb9
Kimd0ng <82995838+Kimd0ng@users.noreply.github.com>
2024-08-23 10:24:29 +0900

    ✨ Feature - axios init (#20)
    * :sparkles: feat: axios init
    
    * :sparkles: feat: axios init

---
+++ FarmHan/api/request.js
@@ -0,0 +1,27 @@
+export const sendRequest = async (instance, method, url, data = {}) => {
+    try {
+        const response = await instance[method](url, data);
+        console.log(
+            `✅${instance.defaults.baseURL} -[${method}] success :`,
+            response,
+        );
+        return response;
+    } catch (error) {
+        console.error(
+            `❌${url}-[${method}] error_response:`,
+            error.response,
+            `error_status : `,
+            error.response.status,
+            `error_status_text: `,
+            error.response.statusText,
+        );
+        throw error;
+    }
+};
+
+export const createUrl = (path, params = {}) => {
+    const query = Object.entries(params)
+        .map(([key, value]) => `${key}=${value}`)
+        .join("&");
+    return `${path}${query ? `?${query}` : ""}`;
+};
