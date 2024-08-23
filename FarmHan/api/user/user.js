ba311ab95d9a348fb1ec808b261d6a040399ccc0
Kimd0ng <82995838+Kimd0ng@users.noreply.github.com>
2024-08-23 11:46:44 +0900

    :ambulance: fix: api init (#24)

--- FarmHan/api/user/user.js
+++ FarmHan/api/user/user.js
@@ -1,10 +1,10 @@
 import { useSetRecoilState } from "recoil";
-import { userRecoilState } from "../../recoil/user/userRecoilState";
+import { exampleState } from "../../recoil/user/userRecoilState";
 import { sendRequest } from "../request";
 import { exampleInstance } from "../instance";
 
 export const useExampleHook = () => {
-    const setExampleState = useSetRecoilState(userRecoilState);
+    const setExampleState = useSetRecoilState(exampleState);
      const exampleGet = async (exampleData) => {
          const response = await sendRequest(exampleInstance, "get", "/", exampleData);
          setExampleState(response.data);
