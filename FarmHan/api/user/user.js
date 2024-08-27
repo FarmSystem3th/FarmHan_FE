f19ce3b0c06fb362c1bb308d1ab31a2870b05795
Gu-nuu <77003772+Gu-nuu@users.noreply.github.com>
2024-08-27 16:15:37 +0900

    feat: MyPage API (#34)

--- FarmHan/api/user/user.js
+++ FarmHan/api/user/user.js
@@ -1,10 +1,36 @@
 import { useSetRecoilState } from "recoil";
 import { userIdState } from "../../recoil/user/userRecoilState";
-import { sendRequest } from "../request";
+import { sendRequest, createUrl } from "../request";
 import { userInstance } from "../instance";
 
 export const useUserHook = () => {
     const setUserIdState = useSetRecoilState(userIdState);
+
+    const signUpUser = async (userData) => {
+        const url = createUrl("/signup"); // 쿼리 파라미터 없이 단순 경로를 생성
+
+        try {
+            const response = await sendRequest(userInstance, "post", url, userData);
+            console.log("회원가입 성공:", response.data);
+            return response.data;
+        } catch (error) {
+            console.error("회원가입 실패:", error);
+            throw error;
+        }
+    };
+
+    const myPageUser = async (userId) => {
+        const url = createUrl(`/mypage/${userId}`);
+
+        try {
+            const response = await sendRequest(userInstance, "get", url);
+            console.log("마이페이지 조회 성공:", response.data);
+            return response.data;
+        } catch (error) {
+            console.error("마이페이지 조회 실패:", error);
+            throw error;
+        }
+
      const loginUser = async (loginId, loginPassword) => {
          try {
              const response = await sendRequest(userInstance, "post", "/login", {
@@ -24,5 +50,7 @@ export const useUserHook = () => {
 
     return {
         loginUser,
-    }
+        signUpUser,
+        myPageUser,
+    };
 };
